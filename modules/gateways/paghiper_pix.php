94743fec7cbe8bf8932298753d593e4c5e451847
Henrique Cruz <eu@henriquecruz.com.br>
2020-12-12 05:16:58 -0300

    Pix implementation scaffolding

---
+++ modules/gateways/paghiper_pix.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * PagHiper - Módulo oficial para integração com WHMCS
+ * 
+ * @package    PagHiper para WHMCS
+ * @version    2.1
+ * @author     Equipe PagHiper https://github.com/paghiper/whmcs
+ * @author     Desenvolvido e mantido Henrique Cruz - https://henriquecruz.com.br/
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2017-2019, PagHiper
+ * @link       https://www.paghiper.com/
+ */
