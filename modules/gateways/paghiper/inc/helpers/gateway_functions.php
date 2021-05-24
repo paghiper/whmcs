62a027405344e768d712895804c9134bc0dd7eb6
emanuellopess <lopes.emanuel@linknacional.com>
2021-05-24 16:18:44 -0300

    implementacao de verificacao do cpf|cnpj e adicao de mensagem de erro

--- modules/gateways/paghiper/inc/helpers/gateway_functions.php
+++ modules/gateways/paghiper/inc/helpers/gateway_functions.php
@@ -667,3 +667,106 @@ function create_paghiper_table() {
         return true;
     }
 }
+
+/**
+ * Valida o número de CPF offline
+ * 
+ * @param string $cpf
+ * 
+ * @return bool $result
+ * 
+ */
+function paghiper_valida_cpf($cpf) {
+    // Extrai somente os números
+    $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+
+    // Verifica se foi informado todos os digitos corretamente
+    if (strlen($cpf) != 11) {
+        return false;
+    }
+
+    // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+    if (preg_match('/(\d)\1{10}/', $cpf)) {
+        return false;
+    }
+
+    // Faz o calculo para validar o CPF
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+        if ($cpf[$c] != $d) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * Valida o número de CNPJ offline
+ * 
+ * @param string $cnpj
+ * 
+ * @return bool $result
+ * 
+ */
+function paghiper_valida_cnpj($cnpj) {
+    // Verifica se um número foi informado
+    if (empty($cnpj)) {
+        return false;
+    }
+
+    // Elimina possivel mascara
+    $cnpj = preg_replace('/[^0-9]/', '', $cnpj);
+    $cnpj = str_pad($cnpj, 14, '0', STR_PAD_LEFT);
+
+    // Verifica se o numero de digitos informados é igual a 14
+    if (strlen($cnpj) != 14) {
+        return false;
+    }
+
+    // Verifica se nenhuma das sequências invalidas abaixo 
+    // foi digitada. Caso afirmativo, retorna falso
+    else {
+        if ($cnpj == '00000000000000' ||
+		$cnpj == '11111111111111' || 
+		$cnpj == '22222222222222' || 
+		$cnpj == '33333333333333' || 
+		$cnpj == '44444444444444' || 
+		$cnpj == '55555555555555' || 
+		$cnpj == '66666666666666' || 
+		$cnpj == '77777777777777' || 
+		$cnpj == '88888888888888' ||
+		$cnpj == '99999999999999') {
+            return false;
+
+        // Calcula os digitos verificadores para verificar se o
+	 // CNPJ é válido
+        } else {
+            $j = 5;
+            $k = 6;
+            $soma1 = '';
+            $soma2 = '';
+
+            for ($i = 0; $i < 13; $i++) {
+                $j = $j == 1 ? 9 : $j;
+                $k = $k == 1 ? 9 : $k;
+
+                $soma2 += ($cnpj[$i] * $k);
+
+                if ($i < 12) {
+                    $soma1 += ($cnpj[$i] * $j);
+                }
+
+                $k--;
+                $j--;
+            }
+
+            $digito1 = $soma1 % 11 < 2 ? 0 : 11 - $soma1 % 11;
+            $digito2 = $soma2 % 11 < 2 ? 0 : 11 - $soma2 % 11;
+
+            return (($cnpj[12] == $digito1) and ($cnpj[13] == $digito2)); // true ou false
+        }
+    }
+}
