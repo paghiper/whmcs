442b9984a2a2d6406872b54d966f7777a006f5a3
Henrique Cruz <eu@henriquecruz.com.br>
2022-06-20 20:05:54 -0300

    Bump plug-inversion and readme.md

--- modules/gateways/paghiper/inc/helpers/attach_pdf_slip.php
+++ modules/gateways/paghiper/inc/helpers/attach_pdf_slip.php
@@ -3,7 +3,7 @@
  * Adiciona boleto bancário como página adicional na fatura anexa no WHMCS
  * 
  * @package    PagHiper para WHMCS
- * @version    2.2.1
+ * @version    2.3
  * @author     Equipe PagHiper https://github.com/paghiper/whmcs
  * @author     Desenvolvido e mantido Henrique Cruz - https://henriquecruz.com.br/
  * @license    BSD License (3-clause)
--- modules/gateways/paghiper/inc/helpers/gateway_functions.php
+++ modules/gateways/paghiper/inc/helpers/gateway_functions.php
@@ -3,7 +3,7 @@
  * PagHiper - Módulo oficial para integração com WHMCS
  * 
  * @package    PagHiper para WHMCS
- * @version    2.2.1
+ * @version    2.3
  * @author     Equipe PagHiper https://github.com/paghiper/whmcs
  * @author     Desenvolvido e mantido Henrique Cruz - https://henriquecruz.com.br/
  * @license    BSD License (3-clause)
